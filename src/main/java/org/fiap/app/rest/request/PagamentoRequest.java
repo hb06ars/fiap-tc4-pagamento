f2e7e9ecf256332cc1130be03c8d8000b54fe006
SISGDSOLAR <32486795+hb06ars@users.noreply.github.com>
2025-03-27 14:34:56 -0300

    a
    a

--- src/main/java/org/fiap/app/rest/request/PagamentoRequest.java
+++ src/main/java/org/fiap/app/rest/request/PagamentoRequest.java
@@ -1,6 +1,5 @@
 package org.fiap.app.rest.request;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,15 +17,13 @@ import java.time.LocalDateTime;
 public class PagamentoRequest {
     private Long id;
 
-    @NotNull(message = "O ID do Produto não pode ser nulo. Por favor, forneça um valor.")
-    @Positive(message = "O ID deve ser maior que 0")
-    private Long produtoId;
     @Positive(message = "O Pedido ID deve ser maior que 0")
     private Long pedidoId;
+
     @Positive(message = "O ID do Pagamento deve ser maior que 0")
     private Long idPagamento;
+
     private LocalDateTime dtPagamento;
     private String statusPagamento;
-    private LocalDateTime dtAtualizacao = LocalDateTime.now();
-
+    private LocalDateTime dtAtualizacao;
 }
