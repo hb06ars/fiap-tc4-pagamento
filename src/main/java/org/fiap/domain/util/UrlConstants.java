b9719cdb3c1f329a3b978ae5820788be57619321
SISGDSOLAR <32486795+hb06ars@users.noreply.github.com>
2025-03-27 15:04:39 -0300

    8087
    8087

--- src/main/java/org/fiap/domain/util/UrlConstants.java
+++ src/main/java/org/fiap/domain/util/UrlConstants.java
@@ -2,5 +2,5 @@ package org.fiap.domain.util;
 
 
 public class UrlConstants {
-    public static final String PEDIDO_BASE_URL = "http://localhost:8086/pedido";
+    public static final String PEDIDO_BASE_URL = "http://localhost:8087/pedido";
 }
