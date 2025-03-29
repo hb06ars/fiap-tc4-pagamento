edf9b9f762bccb13f0a0d743ad711b72deba7eee
SISGDSOLAR <32486795+hb06ars@users.noreply.github.com>
2025-03-29 10:51:33 -0300

    a
    a

--- src/test/java/org/fiap/app/rest/controller/ControllerTest.java
+++ src/test/java/org/fiap/app/rest/controller/ControllerTest.java
@@ -1,5 +1,6 @@
 package org.fiap.app.rest.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.fiap.app.rest.request.PagamentoRequest;
 import org.fiap.domain.dto.PagamentoDTO;
 import org.fiap.domain.enums.StatusPagamentoEnum;
@@ -9,48 +10,52 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.math.BigDecimal;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class ControllerTest {
 
-    @InjectMocks
-    private Controller controller;
+    private MockMvc mockMvc;
 
     @Mock
     private EfetuarPagamentoUseCase efetuarPagamentoUseCase;
 
+    @InjectMocks
+    private Controller pagamentoController;
+
+    private PagamentoDTO pagamentoDTO;
+    private PagamentoRequest pagamentoRequest;
+
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(pagamentoController).build();
+
+        pagamentoDTO = new PagamentoDTO();
+        pagamentoDTO.setId(1L);
+        pagamentoDTO.setPedidoId(100L);
+        pagamentoDTO.setStatusPagamento(StatusPagamentoEnum.ABERTO.getDescricao());
+
+        pagamentoRequest = new PagamentoRequest();
+        pagamentoRequest.setPedidoId(100L);
     }
 
     @Test
-    void testCadastro_Sucesso() {
-        PagamentoRequest request = PagamentoRequest.builder()
-                .pedidoId(1L)
-                .valorTotal(BigDecimal.valueOf(100.00))
-                .build();
-
-        PagamentoDTO pagamentoDTO = PagamentoDTO.builder()
-                .pedidoId(1L)
-                .valorTotal(BigDecimal.valueOf(100.00))
-                .statusPagamento(StatusPagamentoEnum.FECHADO_COM_SUCESSO.getDescricao())
-                .build();
-
+    void testCadastro() throws Exception {
         when(efetuarPagamentoUseCase.execute(any(PagamentoDTO.class))).thenReturn(pagamentoDTO);
 
-        ResponseEntity<PagamentoDTO> response = controller.cadastro(request);
-
-        assertEquals(200, response.getStatusCodeValue());
-        assertEquals(pagamentoDTO, response.getBody());
-        verify(efetuarPagamentoUseCase, times(1)).execute(any(PagamentoDTO.class));
+        mockMvc.perform(post("/pagamento")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(pagamentoRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.pedidoId", is(100)));
     }
 }
